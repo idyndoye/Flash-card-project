43cdfa368cbc77eb261802cd8a76100885d16f6e
idy <idy@wave.com>
2024-12-25 10:49:50 +0000

    adding index

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
     global current_card, flip_timer
     current_card = random.choice(to_learn)
-    print(current_card["French"])
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
@@ -27,9 +32,8 @@ def flip_card():
 
 def is_known():
     to_learn.remove(current_card)
-    print(len(to_learn))
     data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv")
+    data.to_csv("data/words_to_learn.csv", index=False)
 
     next_card()
 
